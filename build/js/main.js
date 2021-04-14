3f6a959b31b9b945dc5c1b1d15b90fc1e257891d
kovarnyi <kovarnyi@gmail.com>
2021-04-14 18:08:48 +0300

    add build

---
+++ build/js/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+let mobileMenuToggle = document.querySelector('.header__menu-toggle')
+let mainNav = document.querySelector('.main-nav')
+
+
+mobileMenuToggle.addEventListener('click', function (evt){
+  mainNav.classList.toggle('open')
+})
